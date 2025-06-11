cbfaea9e56d9186c4057499cb47eede5a67ed043
lihaoyun6 <lihaoyun11@gmail.com>
2025-06-11 17:57:43 +0800

    fix command line tool

--- xh/MakeOverlay.swift
+++ xh/MakeOverlay.swift
@@ -24,9 +24,7 @@ func getActiveWindowGeometry() -> (x: Int, y: Int, width: Int, height: Int)? {
 func openCustomURLWithActiveWindowGeometry(prompt: String = "") {
     if let geometry = getActiveWindowGeometry() {
         if let url = URL(string: "xhistory://show?x=\(geometry.x)&y=\(geometry.y)&w=\(geometry.width)&h=\(geometry.height)\(prompt)") {
-            let config = NSWorkspace.OpenConfiguration()
-            config.activates = false
-            NSWorkspace.shared.open(url, configuration: config)
+            _ = try? NSWorkspace.shared.open(url, options: [.withoutActivation],  configuration: [:] )
         }
     }
 }
